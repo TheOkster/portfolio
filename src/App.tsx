951aba0f5a9c5ba6998db68f7fb465433fbe07f0
Joshua Oki <jokioluda@gmail.com>
2025-03-02 21:52:35 -0500

    Changed Image Structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Project from "./Project";
+import CantonaImage from "./images/Cantona.png";
+import SmoothieImage from "./images/New Smoothie.png";
+import DSImage from "./images/3ds.jpeg";
+import NYT from "./images/NYT.png";
+import WebsiteImage from "./images/Website.png";
 
 function App() {
   return (
@@ -13,20 +18,20 @@ function App() {
         <h1>Joshua Oki's Projects</h1>
       </header>
       <Project
-        image="./portfolio/Images/New Smoothie.png"
+        image={SmoothieImage}
         title="Smoothlie"
         date_string="Jan 2024"
         link="https://github.com/TheOkster/Smoothlie"
         body="An app created as part of a team of 3 for MIT’s web.lab class/competition. This app suggests a schedule for users based off the tasks they have to do and their existing commitments. This wass created using React, Javascript, and MongoDB. I worked on both the front-end and back-end, specifically implementing most of the pages and the MongoDB database."
       />
       <Project
-        image="portfolio/Images/Cantona.png"
+        image={CantonaImage}
         title="Cantona"
         date_string="Mar 2025"
         body="An Android music app created using Jetpack Compose and Kotlin. Still a work-in-progress but the core functionality is working. "
       />
       <Project
-        image="portfolio/Images/3ds.jpeg"
+        image={DSImage}
         title="3DS Game Time Parser"
         date_string="Jan 2024"
         link="https://github.com/TheOkster/3DSActivityLogParser/blob/main/main.py"
@@ -46,13 +51,13 @@ function App() {
         body="A project used to help me compare two images and pick one I found was better without knowing which one was which. This was made using C#."
       />
       <Project
-        image="portfolio/Images/Website.png"
+        image={WebsiteImage}
         title="This Website!"
         date_string="Mar 2, 2024"
         body="Very small project of course but this website was created using very simple React code. Plan to make it prettier soon!"
       />
       <Project
-        image="portfolio/Images/NYT.png"
+        image={NYT}
         title="NYT Article Analyzer"
         date_string="Summer 2024"
         body="A very small project, but was used to help assist me classify articles as living wage related for a work project. This was made using Swift."
